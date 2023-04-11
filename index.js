b59bc56ef719f7cfda754bfbb47f3d21e1eb7d6f
Nesto Gradillas <127235690+NestoG@users.noreply.github.com>
2023-04-10 21:27:59 -0700

    Screen shows user input and calculations.

--- index.js
+++ index.js
@@ -1,6 +1,35 @@
 let firstOperand;
 let operator;
 let secondOperand;
+let numCount = false;
+
+const screen = document.querySelector(".screen");
+let displayVariable="";
+let operandVariable="";
+
+const buttons = document.querySelectorAll('button');
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        if(button.id == "clear") {
+            displayVariable = "";
+        } else if(button.id == "/" ||button.id == "*" || button.id == "-" || button.id == "+") {
+            operator = button.id;
+            firstOperand = displayVariable;
+            numCount = true;
+        } else if(button.id == "="){
+            secondOperand = displayVariable;
+            let calc = operate(firstOperand, operator, secondOperand)
+            displayVariable = calc;
+        } else {
+            if(numCount == true){
+                displayVariable = "";
+                numCount == false;
+            }
+        displayVariable += button.id;
+        }
+        screen.innerText =displayVariable;
+    })
+})
 
 
 function operate(num1, oper, num2){
@@ -13,7 +42,7 @@ function operate(num1, oper, num2){
     if(oper == "-") {
         return subtract(num1, num2);
     }
-    if(oper == "*") {
+    if(oper == "/") {
         return divide(num1, num2);
     }
 }
@@ -24,6 +53,8 @@ function multiply(num1, num2) {
 }
 
 function add(num1, num2) {
+    num1 = +num1;
+    num2 = +num2;
     return num1 + num2;
 }
 
