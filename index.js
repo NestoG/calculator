80be1cba30a1ceec638cf22745a21423ba225992
Nesto Gradillas <127235690+NestoG@users.noreply.github.com>
2023-04-10 20:02:45 -0700

    Initialize operate/calculate function

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
+let firstOperand;
+let operator;
+let secondOperand;
+
+
+function operate(num1, oper, num2){
+    if(oper == "*") {
+        return multiply(num1, num2);
+    }
+    if(oper == "+") {
+        return add(num1, num2);
+    }
+    if(oper == "-") {
+        return subtract(num1, num2);
+    }
+    if(oper == "*") {
+        return divide(num1, num2);
+    }
+}
+
+
 function multiply(num1, num2) {
     return num1 * num2;
 }
